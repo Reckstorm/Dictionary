462f461f480b0228fb0eefec9a859f1f58022392
Mihail Sevagin <47269465+Reckstorm@users.noreply.github.com>
2022-11-01 16:47:21 +0200

    Add files via upload

---
+++ MyDict.cs
@@ -0,0 +1,143 @@
+﻿namespace Classwork_29._10._2022
+{
+    internal class MyDict : IDisposable
+    {
+        public Dictionary<string, List<string>> dict { get; set; }
+
+        private string path;
+        public MyDict(string path = "dict.txt")
+        {
+            this.path = path;
+            if (File.Exists(path))
+            {
+                dict = new Dictionary<string, List<string>>(ReadDict(path));
+            }
+            else
+            {
+                dict = new Dictionary<string, List<string>>();
+            }
+        }
+
+        public string AddWord(string key, List<string> value)
+        {
+            string str = $"Word {key} already exists";
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, value);
+                str = $"Word {key} successfuly added";
+            }
+            return str;
+        }
+
+        public string AddTranslation(string key, string translation)
+        {
+            string str = $"Translation {translation} already exists";
+            if (dict.ContainsKey(key))
+            {
+                if (!dict[key].Contains(translation))
+                {
+                    dict[key].Add($"{translation}");
+                    str = "Translation successfuly added";
+                }
+            }
+            else
+            {
+                AddWord(key, new List<string> { $"{translation}" });
+                str = $"No relevant word. New word {key} has been added";
+            }
+            return str;
+        }
+        public string FindTranslationByKey(string key)
+        {
+            string str = "No relevant value";
+            if (dict.ContainsKey(key))
+            {
+                str = $"Translation for {key} is: ";
+                foreach (var item in dict[key])
+                {
+                    str += item;
+                }
+            }
+            return str;
+        }
+
+        public string FindTranslationByValue(string value)
+        {
+            string str = "No relevant value";
+            foreach (var item in dict)
+            {
+                if (item.Value.Contains(value))
+                {
+                    str = $"Translation for {value} is: {item.Key}";
+                }
+            }
+            return str;
+        }
+
+        public string RemovePairByKey(string key)
+        {
+            string str = "No relevant value";
+            if (dict.ContainsKey(key))
+            {
+                dict.Remove(key);
+                str = $"Pair {key} removed";
+            }
+            return str;
+        }
+
+        public string RemovePairByValue(string value)
+        {
+            string str = "No relevant item";
+            foreach (var item in dict)
+            {
+                if (item.Value.Contains(value))
+                {
+                    str = $"Translation {value} removed";
+                    item.Value.Remove(value);
+                }
+            }
+            return str;
+        }
+
+        private Dictionary<string, List<string>> ReadDict(string path)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            foreach (var item in File.ReadLines(path))
+            {
+                if (item != "")
+                {
+                    var tempStr = item.Split(':');
+                    List<string> tempList = new List<string>(tempStr[1].Split(' '));
+                    dict.Add(tempStr[0], tempList);
+                }
+            }
+            return dict;
+        }
+
+        private void SaveDict()
+        {
+            File.WriteAllText(path, "");
+            foreach (var item in dict)
+            {
+                File.AppendAllText(path, $"{item.Key}:");
+                foreach (var innerItem in item.Value)
+                {
+                    if (item.Value.IndexOf(innerItem) != item.Value.Count-1)
+                    {
+                        File.AppendAllText(path, $"{innerItem} ");
+                    }
+                    else
+                    {
+                        File.AppendAllText(path, $"{innerItem}");
+                    }
+                }
+                File.AppendAllText(path, "\n");
+            }
+        }
+
+        public void Dispose()
+        {
+            SaveDict();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Classwork_29._10._2022;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+MyDict myDict = new MyDict();
+
+Console.WriteLine(myDict.AddWord("Apple", new List<string>() { "Яблоко" }));
+Console.WriteLine(myDict.AddWord("Orange", new List<string>() { "Апельсин", "Оранжевый" }));
+Console.WriteLine(myDict.AddWord("Pineapple", new List<string>() { "Ананас" }));
+Console.WriteLine(myDict.AddWord("Tomato", new List<string>() { "Помидор", "Томат" }));
+Console.WriteLine();
+
+using (myDict)
+{
+    Print(myDict);
+
+    Console.WriteLine(myDict.AddWord("Beetroot", new List<string>() { "Свекла" }));
+    Console.WriteLine(myDict.AddTranslation("Beetroot", "Буряк"));
+    Console.WriteLine(myDict.AddTranslation("Beetroot", "Буряк"));
+    Console.WriteLine();
+
+    Print(myDict);
+
+    Console.WriteLine();
+    Console.WriteLine("Find Translation By Key");
+    Console.WriteLine(myDict.FindTranslationByKey("Apple"));
+    Console.WriteLine();
+    Console.WriteLine("Find Translation By Value");
+    Console.WriteLine(myDict.FindTranslationByValue("Яблоко"));
+    Console.WriteLine();
+    Console.WriteLine("Remove Pair By Key");
+    Console.WriteLine(myDict.RemovePairByKey("Apple"));
+    Console.WriteLine();
+    Console.WriteLine("Remove Translation By Value");
+    Console.WriteLine(myDict.RemovePairByValue("Буряк"));
+
+    Console.WriteLine();
+    Print(myDict);
+}
+
+
+void Print(MyDict someDict)
+{
+    foreach (var item in someDict.dict)
+    {
+        Console.WriteLine($"{item.Key}: ");
+        foreach (var innerItem in item.Value)
+        {
+            Console.WriteLine(innerItem);
+        }
+        Console.WriteLine();
+    }
+}
